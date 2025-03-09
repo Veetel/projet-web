0850f2edfbca8ff4334f30a2e1972e7ef9278983
jeanbaptiste.largeron@gmail.com <veeniux@DESKTOP-CRVE3FA.localdomain>
2025-03-09 18:38:25 +0100

    1er regex de ma vie

--- www/src/a.js
+++ www/src/a.js
@@ -6,19 +6,33 @@ sub.addEventListener("click",get);
 
 function get(){
 
-    let prenom = document.getElementById("Prenom");
-    let nom = document.getElementsByName("Nom")[0].value;
-    let date = document.getElementsByName("Date")[0].value;
-    let password = document.getElementById("password").value;
-    let mail = document.getElementsByName("mail")[0].value;
 
-    if(prenom.value == "" || nom =="" || date == "" ){
-        alert("le prenom, la date ou le nom est vide")
+    let regexMail = /^\S+@\S+\.\S+$/;
+
+
+
+
+    let prenom = document.getElementById("Prenom");
+    let nom = document.getElementsByName("Nom")[0];
+    let date = document.getElementsByName("Date")[0];
+    let password = document.getElementById("password");
+    let mail = regexMail.match(document.getElementsByName("mail")[0].value);
+    
+  
+    
+
+    if(prenom.value == "" || nom.value =="" || date.value == "" ){
+        alert("le prenom, la date ou le nom ou le prenom est vide")
     }
 
-    if(password.length < 12){
+    if(password.length < 12 || password.value == ""){
         alert("mot de passe trop court");
     }
+
+    if(regexMail.mail.value){
+
+    }
+   
 
     console.log(password);
     alert(password);
