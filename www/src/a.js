41dc785aa2ea0b12ad6f69d0e2d363afcd9ce891
jeanbaptiste.largeron@gmail.com <veeniux@DESKTOP-CRVE3FA.localdomain>
2025-03-13 19:41:49 +0100

    fin du formulaire: operationnel

--- www/src/a.js
+++ www/src/a.js
@@ -4,37 +4,76 @@ sub.addEventListener("click",get);
 
 
 
+
+
 function get(){
 
 
-    let regexMail = /^\S+@\S+\.\S+$/;
-
-
-
-
-    let prenom = document.getElementById("Prenom");
-    let nom = document.getElementsByName("Nom")[0];
-    let date = document.getElementsByName("Date")[0];
-    let password = document.getElementById("password");
-    let mail = regexMail.match(document.getElementsByName("mail")[0].value);
-    
-  
+    let regexMail =/^\S+@\S+.\S+$/;
+    let regexPwd = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{12,}$/;
     
 
-    if(prenom.value == "" || nom.value =="" || date.value == "" ){
+
+
+
+    let firstname = document.getElementById("Prenom");
+    let lastname = document.getElementsByName("Nom")[0];
+    let birthdate = document.getElementsByName("Date")[0];
+    let userpwd = document.getElementById("password");
+    let useremail = document.getElementsByName("mail")[0];
+    
+    let a = Date.parse(birthdate.value);
+    
+
+    if(firstname.value == "" || lastname.value =="" || birthdate.value == "" ){
         alert("le prenom, la date ou le nom ou le prenom est vide")
+        return;
     }
 
-    if(password.length < 12 || password.value == ""){
-        alert("mot de passe trop court");
+    if(!regexPwd.test(userpwd.value)){
+        alert("mot de passe invalide");
     }
 
-    if(regexMail.mail.value){
+    if(isNaN(a)){
 
+        console.log("aie");
+
+        alert("Date invalide");
+
+    }
+
+    if(!regexMail.test(useremail.value) ){
+        alert("Email invalide");
     }
    
 
-    console.log(password);
-    alert(password);
+    let formData = {
+        firstname : firstname.value,
+        useremail: useremail.value,
+        userpwd: userpwd.value,
+        lastname: lastname.value,
+        birthdate: birthdate.value
+    };
+
+
+    fetch("../htbin/register.py",{ 
+        method: "POST",
+        headers: {"Content-Type":"application/json"},
+        body:JSON.stringify(formData)
+    })
+
+    .then((response) =>{
+        
+        if(response.ok){
+            window.location.href ="../index.html";
+
+        }
+        else {
+            alert("Une erreur est survenue");
+        }})
+
+    .catch(error => {console.error("Error :", error)});
+
+    console.log(Date.parse(birthdate.value));
 
 }
